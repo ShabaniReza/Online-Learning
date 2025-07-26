13da62d1da40e7653c9b7b3c23de0ac88e856ef2
Reza Shabani <rezashabani.27.1379@gmail.com>
2025-07-26 13:31:56 +0330

    adding routers

--- courses/urls.py
+++ courses/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 router = routers.DefaultRouter()
 router.register('courses', views.CourseViewSet)
+router.register('instructors', views.InstructorViewSet)
+router.register('students', views.StudentViewSet)
+
 
 courses_router = routers.NestedDefaultRouter(router, 'courses', lookup='course')
 courses_router.register('lessons', views.LessonViewSet, basename='course-lessons')
